4705cc350337456a43e572dcfb9b7058c7837992
yang <youngerstar@yang.com>
2021-12-19 19:56:56 +0800

    git test commit!

---
+++ src/main/java/com/yang/gittest/gittest.java
@@ -0,0 +1,8 @@
+package com.yang.gittest;
+
+public class gittest {
+    public static void main(String[] args) {
+        System.out.println("test commit!");
+        System.out.println("test commit!");
+    }
+}
