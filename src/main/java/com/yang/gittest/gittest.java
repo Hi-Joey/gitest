0dadc6f0e499d92f3df824688a4c5a0204d351e4
yang <youngerstar@yang.com>
2021-12-19 19:59:24 +0800

    git test commit2!

--- src/main/java/com/yang/gittest/gittest.java
+++ src/main/java/com/yang/gittest/gittest.java
@@ -4,5 +4,6 @@ public class gittest {
     public static void main(String[] args) {
         System.out.println("test commit!");
         System.out.println("test commit!");
+        System.out.println("here is a test commit2!");
     }
 }
